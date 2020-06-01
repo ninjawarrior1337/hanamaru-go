bdb60846f47038ef72bf2085fabce590292a8fdf
ninjawarrior1337 <t.rothled@gmail.com>
2020-05-31 19:00:19 -0700

    getting audio info now based off of youtube-dl

--- hanamaru/voice/voice_context.go
+++ hanamaru/voice/voice_context.go
@@ -6,15 +6,18 @@ import (
 )
 
 func NewContext() *Context {
+	ytdl, _ := NewYTDL("")
 	return &Context{
 		Queues: make(map[string]*Queue),
 		VCs:    make(map[string]*discordgo.VoiceConnection),
+		Ytdl:   ytdl,
 	}
 }
 
 type Context struct {
 	Queues map[string]*Queue
 	VCs    map[string]*discordgo.VoiceConnection
+	Ytdl   *Ytdl
 }
 
 type Playable interface {
