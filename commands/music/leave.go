87f079eca975bcd4142fa11b0c7348f11693f9f7
ninjawarrior1337 <t.rothled@gmail.com>
2020-06-02 23:46:02 -0700

    Begin refactor of music and voice

--- commands/music/leave.go
+++ commands/music/leave.go
@@ -9,6 +9,11 @@ var Leave = &hanamaru.Command{
 	Name:               "leave",
 	PermissionRequired: 0,
 	Exec: func(ctx *hanamaru.Context) error {
+		if val, ok := ctx.VoiceContext.QueueChannels[ctx.GuildID]; !ok {
+			return fmt.Errorf("cannot disconnect when im not connected")
+		} else {
+			close(val)
+		}
 		if val, ok := ctx.VoiceContext.VCs[ctx.GuildID]; !ok {
 			return fmt.Errorf("cannot disconnect when im not connected")
 		} else {
