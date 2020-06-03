3d3ca5bc2b4b087a9c3358149f0045d90c137db9
ninjawarrior1337 <t.rothled@gmail.com>
2020-06-02 23:46:02 -0700

    Begin refactor of music and voice

--- commands/music/play.go
+++ commands/music/play.go
@@ -11,14 +11,14 @@ var Play = &hanamaru.Command{
 	Name:               "play",
 	PermissionRequired: 0,
 	Exec: func(ctx *hanamaru.Context) error {
-		vc, ok := ctx.VoiceContext.VCs[ctx.GuildID]
+		_, ok := ctx.VoiceContext.VCs[ctx.GuildID]
 		if !ok {
 			return fmt.Errorf("cannot play when im not connected")
 		}
 
-		queue, ok := ctx.VoiceContext.Queues[ctx.GuildID]
+		queueChan, ok := ctx.VoiceContext.QueueChannels[ctx.GuildID]
 		if !ok {
-			return fmt.Errorf("this isnt supposed to happen what")
+			return fmt.Errorf("this isnt supposed to happen wot")
 		}
 
 		videoUrl, err := ctx.GetArgIndex(0)
@@ -26,19 +26,8 @@ var Play = &hanamaru.Command{
 			return errors.New("please pass in a valid video URL")
 		}
 
-		queue.Push(voice.NewYTSrc(videoUrl, ctx.VoiceContext.Ytdl))
-		//fp, _ := filepath.Abs("assets/test.mp3")
-		//fmt.Println(fp)
-		//queue.Push(&voice.StaticFile{FilePath: fp})
-		if queue.Length() == 1 {
-			song := queue.Pop()
-			//ctx.Reply(fmt.Sprintf("%v", song))
-			_, err := song.Play(vc)
-			if err != nil {
-				return fmt.Errorf("failed to play song: %v", err)
-			}
-			//err = <-doneChan
-		}
+		queueChan <- voice.NewYTSrc(videoUrl, ctx.VoiceContext.Ytdl)
+
 		return nil
 	},
 }
--- hanamaru/voice/youtube.src.go
+++ hanamaru/voice/youtube.src.go
@@ -18,10 +18,10 @@ func NewYTSrc(url string, ytdl *Ytdl) *YoutubeSrc {
 	return &YoutubeSrc{YtUrl: url, Ytdl: ytdl}
 }
 
-func (s *YoutubeSrc) Play(vc *discordgo.VoiceConnection) (*dca.StreamingSession, error) {
+func (s *YoutubeSrc) Play(vc *discordgo.VoiceConnection) (*dca.StreamingSession, chan error, error) {
 	videoInfo, err := s.Ytdl.GetVideoInfo(s.YtUrl)
 	if err != nil {
-		return nil, fmt.Errorf("failed to get video info: %v", err)
+		return nil, nil, fmt.Errorf("failed to get video info: %v", err)
 	}
 	var format Formats
 	for _, f := range videoInfo.Formats {
@@ -31,17 +31,14 @@ func (s *YoutubeSrc) Play(vc *discordgo.VoiceConnection) (*dca.StreamingSession,
 		}
 	}
 	if format.URL == "" {
-		return nil, errors.New("failed to get url")
+		return nil, nil, errors.New("failed to get url")
 	}
 	s.ec, err = dca.EncodeFile(format.URL, defaultOptions)
 	if err != nil {
-		return nil, fmt.Errorf("failed to create encoder: %v", err)
+		return nil, nil, fmt.Errorf("failed to create encoder: %v", err)
 	}
 
 	done := make(chan error)
 	stream := dca.NewStream(s.ec, vc, done)
-	err = <-done
-	fmt.Println(err)
-	fmt.Println(s.ec.FFMPEGMessages())
-	return stream, err
+	return stream, done, nil
 }
