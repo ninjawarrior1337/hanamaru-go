91896fdda15aa73cd53c428b5f1c75d909935719
ninjawarrior1337 <teelar1337@gmail.com>
2020-06-08 23:33:34 -0700

    Last changes before public - Create README.md - Add Jisho kanji capability - Fix command linker

--- go.mod
+++ go.mod
@@ -14,6 +14,7 @@ require (
 	github.com/fogleman/gg v1.3.0
 	github.com/fsnotify/fsnotify v1.4.9 // indirect
 	github.com/gobuffalo/here v0.6.2 // indirect
+	github.com/gocolly/colly v1.2.0
 	github.com/gocolly/colly/v2 v2.0.1
 	github.com/gojp/kana v0.1.0
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0
@@ -31,8 +32,6 @@ require (
 	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
 	github.com/pelletier/go-toml v1.7.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/rs/zerolog v1.18.0 // indirect
-	github.com/rylio/ytdl v0.6.2
 	github.com/smartystreets/assertions v1.1.0 // indirect
 	github.com/spf13/afero v1.2.2 // indirect
 	github.com/spf13/cast v1.3.1 // indirect
---
+++ util/jisho/kanji.go
@@ -0,0 +1,166 @@
+package jisho
+
+import (
+	"fmt"
+	"github.com/gocolly/colly"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+type JLPTLevel string
+
+const (
+	N5 JLPTLevel = "N5"
+	N4 JLPTLevel = "N4"
+	N3 JLPTLevel = "N3"
+	N2 JLPTLevel = "N2"
+	N1 JLPTLevel = "N1"
+)
+
+var exampleRegex = regexp.MustCompile(`(\S+)\s+【(\S+)】\s+(.+)`)
+
+//var radicalRegex = regexp.MustCompile(`(.+)\s(.+)\s\((.+)\)`)
+
+type ReadingExample struct {
+	Example string
+	Reading string
+	Meaning string
+}
+
+type Radical struct {
+	Meaning string
+	Symbol  string
+}
+
+type Kanji struct {
+	found              bool
+	Rune               rune
+	TaughtIn           string
+	JLPT               JLPTLevel
+	NewspaperFreqRank  int
+	StrokeCount        int
+	Meaning            string
+	Kunyomi            []string
+	KunyomiExamples    []*ReadingExample
+	Onyomi             []string
+	OnyomiExamples     []*ReadingExample
+	Radical            *Radical
+	Parts              []string
+	StrokeOrderDiagram string
+	JishoUri           string
+}
+
+func parseExample(exampleRaw string) *ReadingExample {
+	res := exampleRegex.FindAllStringSubmatch(exampleRaw, -1)
+	if len(res) > 0 {
+		var re ReadingExample
+		re.Example = res[0][1]
+		re.Reading = res[0][2]
+		re.Meaning = res[0][3]
+		return &re
+	}
+	return nil
+}
+
+func (j *Jisho) SearchForKanji(kanji string) (*Kanji, error) {
+	rel := "/" + kanji + "%20%23kanji"
+	u := j.BaseScrapeURI + rel
+	c := colly.NewCollector()
+
+	var k Kanji
+
+	//Found
+	c.OnHTML("#result_area > div > div:nth-child(1) > div.small-12.large-2.columns > div > div:nth-child(1) > h1", func(element *colly.HTMLElement) {
+		if element.Text == "" {
+			k.found = false
+		} else {
+			k.found = true
+		}
+	})
+
+	//Rune
+	c.OnHTML("#result_area > div > div:nth-child(1) > div.small-12.large-2.columns > div > div:nth-child(1) > h1", func(element *colly.HTMLElement) {
+		k.Rune = []rune(element.Text)[0]
+	})
+
+	//Taught In
+	c.OnHTML("#result_area > div > div:nth-child(1) > div.small-12.large-10.columns > div > div.small-12.large-5.columns > div > div.grade > strong", func(element *colly.HTMLElement) {
+		k.TaughtIn = element.Text
+	})
+
+	//JLPT
+	c.OnHTML("#result_area > div > div:nth-child(1) > div.small-12.large-10.columns > div > div.small-12.large-5.columns > div > div.jlpt > strong", func(element *colly.HTMLElement) {
+		k.JLPT = JLPTLevel(element.Text)
+	})
+
+	//Newspaper Freq
+	c.OnHTML("#result_area > div > div:nth-child(1) > div.small-12.large-10.columns > div > div.small-12.large-5.columns > div > div.frequency > strong", func(element *colly.HTMLElement) {
+		k.NewspaperFreqRank, _ = strconv.Atoi(element.Text)
+	})
+
+	//Stroke Count
+	c.OnHTML("#result_area > div > div:nth-child(1) > div.small-12.large-2.columns > div > div:nth-child(2) > div.kanji-details__stroke_count > strong", func(element *colly.HTMLElement) {
+		k.StrokeCount, _ = strconv.Atoi(element.Text)
+	})
+
+	//Meaning
+	c.OnHTML("#result_area > div > div:nth-child(1) > div.small-12.large-10.columns > div > div.small-12.large-7.columns.kanji-details__main > div.kanji-details__main-meanings", func(element *colly.HTMLElement) {
+		k.Meaning = strings.TrimSpace(element.Text)
+	})
+
+	//Kunyomi
+	c.OnHTML("#result_area > div > div:nth-child(1) > div.small-12.large-10.columns > div > div.small-12.large-7.columns.kanji-details__main > div.kanji-details__main-readings > dl.dictionary_entry.kun_yomi > dd", func(element *colly.HTMLElement) {
+		element.ForEach("a", func(i int, element *colly.HTMLElement) {
+			k.Kunyomi = append(k.Kunyomi, element.Text)
+		})
+	})
+
+	//Kunyomi Examples
+	c.OnHTML("#result_area > div > div:nth-child(3) > div.small-12.large-10.columns > div > div > div.row.compounds > div:nth-child(2) > ul", func(element *colly.HTMLElement) {
+		element.ForEach("li", func(i int, element *colly.HTMLElement) {
+			k.KunyomiExamples = append(k.KunyomiExamples, parseExample(element.Text))
+		})
+	})
+
+	//Onyomi
+	c.OnHTML("#result_area > div > div:nth-child(1) > div.small-12.large-10.columns > div > div.small-12.large-7.columns.kanji-details__main > div.kanji-details__main-readings > dl.dictionary_entry.on_yomi > dd", func(element *colly.HTMLElement) {
+		element.ForEach("a", func(i int, element *colly.HTMLElement) {
+			k.Onyomi = append(k.Onyomi, element.Text)
+		})
+	})
+
+	//Onyomi Examples
+	c.OnHTML("#result_area > div > div:nth-child(3) > div.small-12.large-10.columns > div > div > div.row.compounds > div:nth-child(1) > ul", func(element *colly.HTMLElement) {
+		element.ForEach("li", func(i int, element *colly.HTMLElement) {
+			k.OnyomiExamples = append(k.OnyomiExamples, parseExample(element.Text))
+		})
+	})
+
+	//Radical
+	c.OnHTML("#result_area > div > div:nth-child(1) > div.small-12.large-2.columns > div > div:nth-child(2) > div:nth-child(2) > dl > dd > span", func(element *colly.HTMLElement) {
+		var r Radical
+		r.Meaning = strings.TrimSpace(element.ChildText("span"))
+		r.Symbol = strings.TrimSpace(strings.Trim(strings.TrimSpace(element.Text), r.Meaning))
+		k.Radical = &r
+	})
+
+	//Parts
+	c.OnHTML("#result_area > div > div:nth-child(1) > div.small-12.large-2.columns > div > div:nth-child(2) > div:nth-child(3) > dl > dd", func(element *colly.HTMLElement) {
+		element.ForEach("a", func(i int, element *colly.HTMLElement) {
+			k.Parts = append(k.Parts, element.Text)
+		})
+	})
+
+	//Stroke Order Diagram
+	k.StrokeOrderDiagram = fmt.Sprintf("%v%v_frames.png", j.StrokeOrderDiagramBaseUri, []rune(kanji)[0])
+
+	//Jisho Page
+	k.JishoUri = u
+
+	c.Visit(u)
+	if !k.found {
+		return nil, &KanjiNotFound{Arg: kanji}
+	}
+	return &k, nil
+}
