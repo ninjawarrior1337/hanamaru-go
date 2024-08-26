5f373229a09eb0fb128158a2a520e17488a38640
ninjawarrior1337 <me@treelar.xyz>
2024-08-26 13:41:16 -0700

    use callPackage to pass allow overwriting nixpkgs version [no ci]

--- flake.nix
+++ flake.nix
@@ -5,27 +5,29 @@
   inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils }:
-    let
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }: let
+    # to work with older version of flakes
+    lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
 
-      # to work with older version of flakes
-      lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
-
-      # Generate a user-friendly version number.
-      version = builtins.substring 0 8 lastModifiedDate;
-
-    in
-    flake-utils.lib.eachDefaultSystem (system: 
-    let 
+    # Generate a user-friendly version number.
+    version = builtins.substring 0 8 lastModifiedDate;
+  in
+    flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs {
         inherit system;
       };
     in {
-
       # Provide some binary packages for selected system types.
-      packages = 
-        rec {
-          hanamaru-go = pkgs.buildGoModule {
+      packages = rec {
+        hanamaru-go = {
+          buildGoModule,
+          rustPlatform,
+        }:
+          buildGoModule {
             pname = "hanamaru-go";
             inherit version;
             src = ./.;
@@ -45,7 +47,10 @@
             ];
 
             buildInputs = [
-              hanamaru-lib
+              (hanamaru-lib
+              {
+                inherit rustPlatform;
+              })
             ];
 
             doCheck = false;
@@ -53,27 +58,31 @@
             vendorHash = "sha256-zmVCsmy/sDo9WCI3n/w8dGeQauDGK96gRhDjXtGZtXo=";
           };
 
-          hanamaru-lib = pkgs.rustPlatform.buildRustPackage {
+        hanamaru-lib = {rustPlatform}:
+          rustPlatform.buildRustPackage {
             pname = "hanamaru-lib";
             inherit version;
             src = ./lib;
             cargoHash = "sha256-64arxp+gfKR4RFEU1VfMwM/lLno2JMThq1hGzYF5sok=";
           };
 
-          hanamaru = pkgs.writeShellScriptBin "hanamaru.sh" ''
-              exec ${hanamaru-go}/bin/hanamaru-go "$@"
-            '';
+        hanamaru = {
+          writeShellScriptBin,
+          rustPlatform,
+          buildGoModule,
+        }: (writeShellScriptBin "hanamaru" ''
+          exec ${hanamaru-go {inherit rustPlatform buildGoModule;}}/bin/hanamaru-go "$@"
+        '');
 
-          default = hanamaru;
-        };
+        default = pkgs.callPackage hanamaru {};
+      };
 
       # Add dependencies that are only needed for development
-      devShells = 
-        {
-          default = pkgs.mkShell {
-            buildInputs = with pkgs; [ go gopls gotools go-tools ];
-          };
+      devShells = {
+        default = pkgs.mkShell {
+          buildInputs = with pkgs; [go gopls gotools go-tools];
         };
+      };
 
       # The default package for 'nix build'. This makes sense if the
       # flake provides only one package or there is a clear "main"
